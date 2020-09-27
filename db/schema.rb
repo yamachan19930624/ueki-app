a7ce1c79afbae1840e90f2b6d116158e75cc5346
山下優樹 <>
2020-09-28 07:39:13 +0900

    product modelを作った

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_220639) do
+ActiveRecord::Schema.define(version: 2020_09_27_222111) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+Product.create!(
+  name: Faker::Book.title,
+  description: Faker::Lorem.paragraph(2),
+  price: Faker::Number.within(100..100000)
+)
+end
